54ab5369d1b1ea234d5569e60289070877c91166
Sharan Babu <50396375+Sharan-Babu@users.noreply.github.com>
2023-05-30 12:15:01 +0530

    Update vidtheta.py

--- vidtheta.py
+++ vidtheta.py
@@ -238,7 +238,7 @@ if page == "Upload":
 
 # Search Videos Page
 elif page == "Search":
-    st.write(video_metadatas_array)
+    #st.write(video_metadatas_array)
 
 
     if len(video_summaries_array) == 0:
